9e710fedab5d3e83539d44a2e0bc5c5de103974f
Alex Hilman <ahilman@ethor.com>
2017-08-07 12:00:10 -0600

    Setting borderless style for container

--- src/main/java/com/alexhilman/cameradashboard/ui/view/ViewContainer.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/view/ViewContainer.java
@@ -3,6 +3,7 @@ package com.alexhilman.cameradashboard.ui.view;
 import com.google.inject.Inject;
 import com.vaadin.guice.annotation.UIScope;
 import com.vaadin.ui.Panel;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
  */
@@ -11,5 +12,6 @@ public class ViewContainer extends Panel {
     @Inject
     ViewContainer() {
         setSizeFull();
+        setStyleName(ValoTheme.PANEL_BORDERLESS);
     }
 }
