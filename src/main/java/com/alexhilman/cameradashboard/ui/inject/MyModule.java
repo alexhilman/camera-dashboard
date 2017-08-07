aeb0b353a2e883d5b755123b344e5cd58482cc67
Alex Hilman <ahilman@ethor.com>
2017-08-07 11:32:19 -0600

    I guess I don't need those explicit bindings

--- src/main/java/com/alexhilman/cameradashboard/ui/inject/MyModule.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/inject/MyModule.java
@@ -1,7 +1,5 @@
 package com.alexhilman.cameradashboard.ui.inject;
 
-import com.alexhilman.cameradashboard.ui.view.Dashboard;
-import com.alexhilman.cameradashboard.ui.view.ErrorView;
 import com.google.inject.AbstractModule;
 
 /**
@@ -9,7 +7,5 @@ import com.google.inject.AbstractModule;
 public class MyModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(Dashboard.class);
-        bind(ErrorView.class);
     }
 }
