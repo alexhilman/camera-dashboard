4e8527a970e4b03aad5ab57a73245ffba7f7a0ce
Alex Hilman <ahilman@ethor.com>
2017-08-07 13:55:24 -0600

    Adding some more validation; switching from a uri-like syntax for the conf dir

--- src/main/java/com/alexhilman/cameradashboard/ui/App.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/App.java
@@ -6,7 +6,9 @@ import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.io.File;
-import java.net.URI;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkState;
@@ -17,6 +19,7 @@ import static com.google.common.base.Preconditions.checkState;
 public class App {
     public static final int PORT = 9090;
     public static final String CAMERAS_CONFIG_FILE = "cameras.json";
+    public static final String DASHBOARD_CONFIG_FILE = "camera-dashboard.properties";
     public static final String CONFIGURATION_DIRECTORY_SYSTEM_PROPERTY = "camdash.configurationDir";
 
     public static void main(String[] args) {
@@ -55,7 +58,7 @@ public class App {
                    "The system property \"" + CONFIGURATION_DIRECTORY_SYSTEM_PROPERTY +
                            "\" must be set to the directory containing the " + CAMERAS_CONFIG_FILE + " configuration file");
 
-        final File confDir = new File(URI.create(configurationDirectory));
+        final File confDir = new File(configurationDirectory);
         checkState(confDir.exists(),
                    "The specified configuration directory \"" + configurationDirectory +
                            "\" does not exist");
@@ -73,6 +76,25 @@ public class App {
                                       .getAbsolutePath()));
 
         checkState(cameraConfigfile.exists(), CAMERAS_CONFIG_FILE + " does not exist");
+
+        final File dashboardConfigFile =
+                Stream.of(nullToEmptyArray(confDir.listFiles()))
+                      .filter(f -> f.getName().equals(DASHBOARD_CONFIG_FILE))
+                      .findFirst()
+                      .orElseThrow(() -> new IllegalStateException(
+                              "The " + DASHBOARD_CONFIG_FILE + " configuration file was not found in " + confDir
+                                      .getAbsolutePath()));
+
+        checkState(dashboardConfigFile.exists(), DASHBOARD_CONFIG_FILE + " file does not exist");
+
+        final Properties tempProperties = new Properties();
+        try (final InputStream inputStream = new FileInputStream(dashboardConfigFile)) {
+            tempProperties.load(inputStream);
+            final String storageLocation = tempProperties.getProperty("cameradashboard.video.location");
+            checkState(!Strings.isNullOrEmpty(storageLocation), "Property not found: cameradashboard.video.location");
+        } catch (Exception e) {
+            throw new IllegalStateException("Invalid properties found in " + DASHBOARD_CONFIG_FILE, e);
+        }
     }
 
     @SuppressWarnings("unchecked")
