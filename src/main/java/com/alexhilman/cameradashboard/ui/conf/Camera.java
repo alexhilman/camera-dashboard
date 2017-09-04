e1f57346d6e33365b52903825f84cf616d1f5ce6
Alex Hilman <alex.hilman@gmail.com>
2017-09-03 22:46:58 -0600

    Making network address part of the camera, not the driver

--- src/main/java/com/alexhilman/cameradashboard/ui/conf/Camera.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/conf/Camera.java
@@ -10,15 +10,34 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class Camera {
     private final String make;
     private final String model;
+    private final String networkAddress;
     private final Driver driver;
 
     @JsonCreator
     public Camera(@JsonProperty(value = "make", required = true) final String make,
                   @JsonProperty(value = "model", required = true) final String model,
+                  @JsonProperty(value = "networkAddress", required = true) final String networkAddress,
                   @JsonProperty(value = "driver", required = true) final Driver driver) {
         this.make = checkNotNull(make, "make cannot be null");
         this.model = checkNotNull(model, "model cannot be null");
+        this.networkAddress = checkNotNull(networkAddress, "networkAddress cannot be null");
         this.driver = checkNotNull(driver, "driver cannot be null");
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getNetworkAddress() {
+        return networkAddress;
+    }
+
+    public Driver getDriver() {
+        return driver;
     }
 
     @Override
@@ -30,6 +49,7 @@ public class Camera {
 
         if (!make.equals(camera.make)) return false;
         if (!model.equals(camera.model)) return false;
+        if (!networkAddress.equals(camera.networkAddress)) return false;
         return driver.equals(camera.driver);
     }
 
@@ -37,6 +57,7 @@ public class Camera {
     public int hashCode() {
         int result = make.hashCode();
         result = 31 * result + model.hashCode();
+        result = 31 * result + networkAddress.hashCode();
         result = 31 * result + driver.hashCode();
         return result;
     }
@@ -46,6 +67,7 @@ public class Camera {
         return "Camera{" +
                 "make='" + make + '\'' +
                 ", model='" + model + '\'' +
+                ", networkAddress='" + networkAddress + '\'' +
                 ", driver=" + driver +
                 '}';
     }
--- src/main/java/com/alexhilman/cameradashboard/ui/conf/Driver.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/conf/Driver.java
@@ -10,15 +10,12 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class Driver {
     private final Type type;
     private final String implementation;
-    private final String networkAddress;
 
     @JsonCreator
     public Driver(@JsonProperty(value = "type", required = true) final Type type,
-                  @JsonProperty(value = "implementation", required = true) final String implementation,
-                  @JsonProperty(value = "networkAddress", required = true) final String networkAddress) {
+                  @JsonProperty(value = "implementation", required = true) final String implementation) {
         this.type = checkNotNull(type, "type cannot be null");
         this.implementation = checkNotNull(implementation, "implementation cannot be null");
-        this.networkAddress = checkNotNull(networkAddress, "networkAddress cannot be null");
     }
 
     public Type getType() {
@@ -29,10 +26,6 @@ public class Driver {
         return implementation;
     }
 
-    public String getNetworkAddress() {
-        return networkAddress;
-    }
-
     @Override
     public boolean equals(final Object o) {
         if (this == o) return true;
@@ -41,15 +34,21 @@ public class Driver {
         final Driver driver = (Driver) o;
 
         if (type != driver.type) return false;
-        if (!implementation.equals(driver.implementation)) return false;
-        return networkAddress.equals(driver.networkAddress);
+        return implementation.equals(driver.implementation);
     }
 
     @Override
     public int hashCode() {
         int result = type.hashCode();
         result = 31 * result + implementation.hashCode();
-        result = 31 * result + networkAddress.hashCode();
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "type=" + type +
+                ", implementation='" + implementation + '\'' +
+                '}';
     }
 }
