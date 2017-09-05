1cf6e25d4f07157be227f53fcb48157abc50fcec
Alex Hilman <alex.hilman@gmail.com>
2017-09-04 22:10:36 -0600

    Adding username and password to the camera config

--- src/main/java/com/alexhilman/cameradashboard/ui/conf/Camera.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/conf/Camera.java
@@ -11,16 +11,22 @@ public class Camera {
     private final String make;
     private final String model;
     private final String networkAddress;
+    private final String username;
+    private final String password;
     private final Driver driver;
 
     @JsonCreator
     public Camera(@JsonProperty(value = "make", required = true) final String make,
                   @JsonProperty(value = "model", required = true) final String model,
                   @JsonProperty(value = "networkAddress", required = true) final String networkAddress,
+                  @JsonProperty("username") final String username,
+                  @JsonProperty("password") final String password,
                   @JsonProperty(value = "driver", required = true) final Driver driver) {
         this.make = checkNotNull(make, "make cannot be null");
         this.model = checkNotNull(model, "model cannot be null");
         this.networkAddress = checkNotNull(networkAddress, "networkAddress cannot be null");
+        this.username = username;
+        this.password = password;
         this.driver = checkNotNull(driver, "driver cannot be null");
     }
 
@@ -34,6 +40,14 @@ public class Camera {
 
     public String getNetworkAddress() {
         return networkAddress;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public Driver getDriver() {
@@ -50,6 +64,8 @@ public class Camera {
         if (!make.equals(camera.make)) return false;
         if (!model.equals(camera.model)) return false;
         if (!networkAddress.equals(camera.networkAddress)) return false;
+        if (username != null ? !username.equals(camera.username) : camera.username != null) return false;
+        if (password != null ? !password.equals(camera.password) : camera.password != null) return false;
         return driver.equals(camera.driver);
     }
 
@@ -58,6 +74,8 @@ public class Camera {
         int result = make.hashCode();
         result = 31 * result + model.hashCode();
         result = 31 * result + networkAddress.hashCode();
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
         result = 31 * result + driver.hashCode();
         return result;
     }
@@ -68,6 +86,8 @@ public class Camera {
                 "make='" + make + '\'' +
                 ", model='" + model + '\'' +
                 ", networkAddress='" + networkAddress + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
                 ", driver=" + driver +
                 '}';
     }
