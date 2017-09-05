0653fd41662ad87b67f715c45945d7bcfd46f055
Alex Hilman <alex.hilman@gmail.com>
2017-09-04 22:15:57 -0600

    Adding the initial file watcher code

---
+++ src/main/java/com/alexhilman/cameradashboard/ui/video/CameraFileWatcher.java
@@ -0,0 +1,68 @@
+package com.alexhilman.cameradashboard.ui.video;
+
+import com.alexhilman.cameradashboard.ui.conf.Camera;
+import com.alexhilman.cameradashboard.ui.conf.CameraConfiguration;
+import com.alexhilman.dlink.dcs936.AccessCredentials;
+import com.alexhilman.dlink.dcs936.Dcs936Client;
+import com.alexhilman.dlink.dcs936.model.DcsFile;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ */
+@Singleton
+public class CameraFileWatcher {
+    private CameraConfiguration cameraConfiguration;
+
+    @Inject
+    public CameraFileWatcher(final CameraConfiguration cameraConfiguration) {
+        this.cameraConfiguration = checkNotNull(cameraConfiguration, "cameraConfiguration cannot be null");
+    }
+
+    List<Camera> getCameras() {
+        return cameraConfiguration.getCameras();
+    }
+
+    CameraSelector select(final Camera camera) {
+        return new CameraSelector(camera);
+    }
+
+    public class CameraSelector {
+        private final Camera camera;
+
+        private CameraSelector(final Camera camera) {
+            this.camera = camera;
+        }
+
+        public List<DcsFile> listFilesNewerThan(final Instant instant) {
+            return driverFor(camera).findNewMoviesSince(instant);
+        }
+    }
+
+    //TODO this needs to be interfaced properly; usernames and passwords should be universal, etc
+    private Dcs936Client driverFor(final Camera camera) {
+        if (!camera.getDriver().getImplementation().equals(Dcs936Client.class.getName())) {
+            throw new IllegalArgumentException("Camera driver not supported: " + camera.getDriver()
+                                                                                       .getImplementation());
+        }
+
+        try {
+            return new Dcs936Client(
+                    new AccessCredentials(camera.getUsername(),
+                                          camera.getPassword(),
+                                          new URL(camera.getNetworkAddress())
+                    )
+            );
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Malformed URL for camera: " + camera.getNetworkAddress(), e);
+        }
+    }
+}
---
+++ src/test/java/com/alexhilman/cameradashboard/ui/video/CameraFileWatcherIT.java
@@ -0,0 +1,53 @@
+package com.alexhilman.cameradashboard.ui.video;
+
+import com.alexhilman.cameradashboard.ui.conf.Camera;
+import com.alexhilman.cameradashboard.ui.conf.CameraConfiguration;
+import com.alexhilman.dlink.dcs936.model.DcsFile;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class CameraFileWatcherIT {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private CameraFileWatcher cameraFileWatcher;
+
+    @Before
+    public void setup() {
+        final CameraConfiguration cameraConfiguration;
+        try {
+            cameraConfiguration =
+                    OBJECT_MAPPER.readValue(getClass().getResource("/com/alexhilman/cameradashboard/ui/cameras.json"),
+                                            CameraConfiguration.class);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
+        cameraFileWatcher = new CameraFileWatcher(cameraConfiguration);
+    }
+
+    @Test
+    public void shouldGetCameras() {
+        final List<Camera> cameras = cameraFileWatcher.getCameras();
+
+        assertThat(cameras, hasSize(1));
+    }
+
+    @Test
+    public void shouldListNewFilesSinceInstant() {
+        final Instant instant = Instant.now().minus(3, ChronoUnit.HOURS);
+        final Camera camera = cameraFileWatcher.getCameras().get(0);
+        final List<DcsFile> files = cameraFileWatcher.select(camera)
+                                                     .listFilesNewerThan(instant);
+
+        assertThat(files, is(notNullValue()));
+        assertThat(files, hasSize(greaterThanOrEqualTo(1)));
+    }
+}
