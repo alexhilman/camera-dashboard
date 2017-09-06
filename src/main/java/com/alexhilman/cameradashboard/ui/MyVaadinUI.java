6a3e4d165b998647935398ab0ab278c75ace0223
Alex Hilman <alex.hilman@gmail.com>
2017-09-05 22:36:57 -0600

    Should work for starting the movie watching...

--- src/main/java/com/alexhilman/cameradashboard/ui/MyVaadinUI.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/MyVaadinUI.java
@@ -2,9 +2,11 @@ package com.alexhilman.cameradashboard.ui;
 
 import com.alexhilman.cameradashboard.ui.inject.CameraDashboardInitializer;
 import com.alexhilman.cameradashboard.ui.layout.RootLayout;
+import com.alexhilman.cameradashboard.ui.video.CameraMovieWatcher;
 import com.alexhilman.cameradashboard.ui.view.ErrorView;
 import com.alexhilman.cameradashboard.ui.view.ViewContainer;
 import com.google.inject.Inject;
+import com.google.inject.Injector;
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
@@ -15,7 +17,10 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.ui.Transport;
 import com.vaadin.ui.UI;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import java.lang.reflect.Field;
 
 @Theme("mytheme")
 @GuiceUI(viewContainer = ViewContainer.class, path = "/", errorView = ErrorView.class)
@@ -35,5 +40,20 @@ public class MyVaadinUI extends UI {
     @PackagesToScan(value = "com.alexhilman.cameradashboard.ui")
     @CameraDashboardInitializer
     public static class MyVaadinServlet extends GuiceVaadinServlet {
+        @Override
+        public void init(final ServletConfig servletConfig) throws ServletException {
+            super.init(servletConfig);
+
+            final Injector injector;
+            try {
+                final Field injectorField = GuiceVaadinServlet.class.getDeclaredField("injector");
+                injectorField.setAccessible(true);
+                injector = (Injector) injectorField.get(this);
+            } catch (Exception e) {
+                throw new IllegalStateException("Cannot get the injector");
+            }
+
+            injector.getInstance(CameraMovieWatcher.class).start();
+        }
     }
 }
--- src/main/java/com/alexhilman/cameradashboard/ui/video/CameraMovieWatcher.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/video/CameraMovieWatcher.java
@@ -5,17 +5,20 @@ import com.alexhilman.cameradashboard.ui.conf.CameraConfiguration;
 import com.alexhilman.dlink.dcs936.AccessCredentials;
 import com.alexhilman.dlink.dcs936.Dcs936Client;
 import com.alexhilman.dlink.dcs936.model.DcsFile;
-import com.alexhilman.dlink.helper.IOStreams;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Instant;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -23,14 +26,42 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 @Singleton
 public class CameraMovieWatcher {
+    private static final Logger LOG = LogManager.getLogger(CameraMovieWatcher.class);
+
     private final CameraConfiguration cameraConfiguration;
     private final MovieFileManager movieFileManager;
+    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    private volatile boolean running;
 
     @Inject
     public CameraMovieWatcher(final CameraConfiguration cameraConfiguration,
                               final MovieFileManager movieFileManager) {
         this.cameraConfiguration = checkNotNull(cameraConfiguration, "cameraConfiguration cannot be null");
         this.movieFileManager = movieFileManager;
+    }
+
+    public void start() {
+        boolean running = this.running;
+        if (!running) {
+            synchronized (this) {
+                running = this.running;
+                if (!running) {
+                    executorService.scheduleWithFixedDelay(
+                            () -> {
+                                try {
+                                    downloadNewFiles();
+                                } catch (Exception e) {
+                                    LOG.error("Could not fully iterate through new files", e);
+                                }
+                            },
+                            0,
+                            5,
+                            TimeUnit.SECONDS
+                    );
+                }
+                running = true;
+            }
+        }
     }
 
     List<Camera> getCameras() {
@@ -57,6 +88,8 @@ public class CameraMovieWatcher {
 
     public void downloadNewFiles() {
         cameraConfiguration.getCameras().forEach(camera -> {
+            LOG.info("Looking for new files in {} at {}", camera.getName(), camera.getNetworkAddress());
+
             final Dcs936Client dcs936Client = driverFor(camera);
 
             final Instant lastInstant = movieFileManager.lastMovieInstantFor(camera);
