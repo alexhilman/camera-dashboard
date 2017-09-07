74e887183a80dd1e59b2b8c6c0c0bb6231228f82
Alex Hilman <alex.hilman@gmail.com>
2017-09-06 22:05:40 -0600

    Adding a hack to allow the vaadin resources to be accessed; ugh deprecated methods...

--- src/main/java/com/alexhilman/cameradashboard/ui/MyVaadinUI.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/MyVaadinUI.java
@@ -14,13 +14,17 @@ import com.vaadin.guice.annotation.GuiceUI;
 import com.vaadin.guice.annotation.PackagesToScan;
 import com.vaadin.guice.server.GuiceVaadinServlet;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.shared.ui.ui.Transport;
 import com.vaadin.ui.UI;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
+import java.net.URL;
 
 @Theme("mytheme")
 @GuiceUI(viewContainer = ViewContainer.class, path = "/", errorView = ErrorView.class)
@@ -40,6 +44,41 @@ public class MyVaadinUI extends UI {
     @PackagesToScan(value = "com.alexhilman.cameradashboard.ui")
     @CameraDashboardInitializer
     public static class MyVaadinServlet extends GuiceVaadinServlet {
+        private static final org.slf4j.Logger getLogger() {
+            return LoggerFactory.getLogger(VaadinServlet.class);
+        }
+
+        @Override
+        protected boolean isAllowedVAADINResourceUrl(HttpServletRequest request, URL resourceUrl) {
+            if ("jar".equals(resourceUrl.getProtocol())) {
+                if (!resourceUrl.getPath().contains("!/VAADIN/")) {
+                    getLogger().warn("Blocked attempt to access a JAR entry not starting with /VAADIN/: " + resourceUrl);
+                    return false;
+                }
+                getLogger().debug("Accepted access to a JAR entry using a class loader: " + resourceUrl);
+                return true;
+
+            } else if ("rsrc".equals(resourceUrl.getProtocol())) {
+                // This branch was added to allow access to RSRC files that start with "VAADIN/" (not "/VAADIN/")
+                if (!resourceUrl.getPath().contains("VAADIN/")
+                        || resourceUrl.getPath().contains("/../")) {
+                    getLogger().info("Blocked attempt to access resource: " + resourceUrl);
+                    return false;
+                }
+                getLogger().info("Accepted access to a resource using a class loader: " + resourceUrl);
+                return true;
+
+            } else {
+                if (!resourceUrl.getPath().contains("/VAADIN/")
+                        || resourceUrl.getPath().contains("/../")) {
+                    getLogger().info("Blocked attempt to access file: " + resourceUrl);
+                    return false;
+                }
+                getLogger().info("Accepted access to a file using a class loader: " + resourceUrl);
+                return true;
+            }
+        }
+
         @Override
         public void init(final ServletConfig servletConfig) throws ServletException {
             super.init(servletConfig);
