1bf810d7bcab6e7ce41ceb326d8581aa1a49db56
Alex Hilman <alex.hilman@gmail.com>
2017-09-25 08:48:15 -0600

    Adding date grouping in the movie list

--- src/main/java/com/alexhilman/cameradashboard/ui/view/Movies.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/view/Movies.java
@@ -3,30 +3,33 @@ package com.alexhilman.cameradashboard.ui.view;
 import com.alexhilman.cameradashboard.ui.video.Movie;
 import com.alexhilman.cameradashboard.ui.video.MovieFileManager;
 import com.alexhilman.cameradashboard.ui.video.MovieHelper;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.vaadin.guice.annotation.GuiceView;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
 import com.vaadin.server.ExternalResource;
 import com.vaadin.server.Sizeable;
+import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.ui.*;
 
 import java.io.File;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
+import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 
 /**
  */
 @GuiceView("movies")
 public class Movies implements View {
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("LL d, yyyy");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("MMMM d, yyyy", Locale.US);
     private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("h:m:s a");
     private final MovieFileManager movieFileManager;
     private final MovieHelper movieHelper;
@@ -56,35 +59,71 @@ public class Movies implements View {
 
     private Component[] buildPostersFor(final List<Movie> moviesSince) {
         return moviesSince.stream()
-                          .sorted((m1, m2) -> m2.getName().compareTo(m1.getName()))
-                          .map(movie -> {
-                              final HorizontalLayout posterLayout = new HorizontalLayout();
-                              final Image image = new Image(null,
-                                                            new ExternalResource("/movies/" + contextResourceNameFor(
-                                                                    movie.getPosterImageFile())));
-                              image.setWidth(20, Sizeable.Unit.EM);
-                              image.addClickListener(event -> {
-                                  ClassNavigator.navigateTo(WatchMovie.class);
-                              });
-                              posterLayout.addComponent(image);
+                          .collect(groupingBy(movie -> {
+                              return movie.getDateTime().toLocalDate();
+                          }))
+                          .entrySet()
+                          .stream()
+                          .sorted(Comparator.comparing(Map.Entry::getKey))
+                          .map(movieEntry -> {
+                              final LocalDate movieDate = movieEntry.getKey();
+                              final List<Component> dateComponents =
+                                      Lists.newArrayListWithCapacity(movieEntry.getValue().size() + 1);
+                              dateComponents.add(dateSeparatorComponentFor(movieDate));
 
-                              final GridLayout movieDetails = new GridLayout(2, 3);
-                              posterLayout.addComponents(movieDetails);
-                              final LocalDateTime movieDateTime = movie.getDateTime();
-                              movieDetails.addComponents(new Label("Date: "),
-                                                         new Label(movieDateTime.toLocalDate().format(DATE_FORMATTER)));
-                              movieDetails.addComponents(new Label("Time: "),
-                                                         new Label(movieDateTime.toLocalTime().format(TIME_FORMATTER)));
-                              final Duration runningTime = movieHelper.runningLengthFor(movie);
-                              movieDetails.addComponents(new Label("Length: "),
-                                                         new Label(runningTime.toMinutes() + ":" +
-                                                                           runningTime.minus(runningTime.toMinutes(),
-                                                                                             ChronoUnit.MINUTES)
-                                                                                      .getSeconds()));
-                              return posterLayout;
+                              dateComponents.addAll(
+                                      movieEntry.getValue()
+                                                .stream()
+                                                .sorted((m1, m2) -> m2.getName().compareTo(m1.getName()))
+                                                .map(movie -> {
+                                                    final HorizontalLayout posterLayout = new HorizontalLayout();
+                                                    final Image image =
+                                                            new Image(null,
+                                                                      new ExternalResource(
+                                                                              "/movies/" + contextResourceNameFor(
+                                                                                      movie.getPosterImageFile())));
+
+                                                    image.setWidth(20, Sizeable.Unit.EM);
+                                                    image.addClickListener(event -> ClassNavigator.navigateTo(WatchMovie.class));
+                                                    posterLayout.addComponent(image);
+
+                                                    final GridLayout movieDetails = new GridLayout(2, 3);
+                                                    posterLayout.addComponents(movieDetails);
+                                                    final LocalDateTime movieDateTime = movie.getDateTime();
+                                                    movieDetails.addComponents(new Label("Date: "),
+                                                                               new Label(movieDateTime.toLocalDate()
+                                                                                                      .format(DATE_FORMATTER)));
+                                                    movieDetails.addComponents(new Label("Time: "),
+                                                                               new Label(movieDateTime.toLocalTime()
+                                                                                                      .format(TIME_FORMATTER)));
+                                                    final Duration runningTime = movieHelper.runningLengthFor(movie);
+                                                    movieDetails.addComponents(new Label("Length:&nbsp;", ContentMode.HTML),
+                                                                               new Label(runningTime.toMinutes() + ":" +
+                                                                                                 runningTime.minus(
+                                                                                                         runningTime.toMinutes(),
+                                                                                                         ChronoUnit.MINUTES)
+                                                                                                            .getSeconds()));
+                                                    return posterLayout;
+                                                })
+                                                .collect(toList()));
+                              return dateComponents;
                           })
+                          .flatMap(List::stream)
                           .collect(toList())
                           .toArray(new Component[moviesSince.size()]);
+    }
+
+    private Component dateSeparatorComponentFor(final LocalDate date) {
+        final HorizontalLayout components = new HorizontalLayout();
+        components.setWidth(100, Sizeable.Unit.PERCENTAGE);
+        final Label firstRule = new Label("<hr/>", ContentMode.HTML);
+        firstRule.setWidth(100, Sizeable.Unit.PERCENTAGE);
+        final Label secondRule = new Label("<hr/>", ContentMode.HTML);
+        secondRule.setWidth(100, Sizeable.Unit.PERCENTAGE);
+        components.addComponents(firstRule, new Label(date.format(DATE_FORMATTER)), secondRule);
+        components.setExpandRatio(firstRule, 1);
+        components.setExpandRatio(secondRule, 1);
+        return components;
     }
 
     private String contextResourceNameFor(final File file) {
