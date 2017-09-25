651ee12c9a4c90f585535befd4b7d5b12267337f
Alex Hilman <alex.hilman@gmail.com>
2017-09-25 07:53:50 -0600

    Adding some movie tiles to the dashboard

--- src/main/java/com/alexhilman/cameradashboard/ui/view/Dashboard.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/view/Dashboard.java
@@ -1,15 +1,25 @@
 package com.alexhilman.cameradashboard.ui.view;
 
+import com.alexhilman.cameradashboard.ui.video.Movie;
 import com.alexhilman.cameradashboard.ui.video.MovieFileManager;
 import com.google.inject.Inject;
 import com.vaadin.guice.annotation.GuiceView;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.ExternalResource;
 import com.vaadin.server.Sizeable;
 import com.vaadin.ui.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  */
@@ -43,10 +53,6 @@ public class Dashboard implements View {
         return rootLayout;
     }
 
-    private Component buildMoviesComponent() {
-        return new Label("Placeholder");
-    }
-
     @Override
     public void enter(final ViewChangeListener.ViewChangeEvent event) {
         final long usableSpace = movieFileManager.getUsableSpace();
@@ -59,7 +65,33 @@ public class Dashboard implements View {
                                        " used out of " + humanReadableByteCount(totalSpace) +
                                        " (" + humanReadableByteCount(usableSpace) + " free)");
 
-        todayMovies.setContent(buildMoviesComponent());
+        final HorizontalLayout moviePreviewTiles = new HorizontalLayout();
+        todayMovies.setContent(moviePreviewTiles);
+        moviePreviewTiles.addComponents(buildVideoTilesFor(movieFileManager.getMoviesSince(midnightThisMorning())));
+    }
+
+    private Component[] buildVideoTilesFor(final List<Movie> moviesSince) {
+        return moviesSince.stream()
+                          .sorted(Comparator.comparing(Movie::getName))
+                          .map(movie -> {
+                              final Image image = new Image(null,
+                                                            new ExternalResource("/movies/" + contextResourceNameFor(
+                                                                    movie.getPosterImageFile())));
+                              image.setWidth(20, Sizeable.Unit.EM);
+
+                              image.addClickListener(event -> {
+                                  ClassNavigator.navigateTo(WatchMovie.class);
+                              });
+                              return image;
+                          })
+                          .collect(toList())
+                          .toArray(new Component[moviesSince.size()]);
+    }
+
+    private String contextResourceNameFor(final File file) {
+        final String absolutePath = movieFileManager.getStorageDirectory().getAbsolutePath();
+        final String substring = file.getAbsolutePath().substring(absolutePath.length() + 1);
+        return substring;
     }
 
     public static String humanReadableByteCount(long bytes) {
@@ -67,5 +99,15 @@ public class Dashboard implements View {
         if (bytes < unit) return bytes + " B";
         int exp = (int) (Math.log(bytes) / Math.log(unit));
         return String.format("%.1f %sB", bytes / Math.pow(unit, exp), "kMGTPE".charAt(exp - 1));
+    }
+
+    private Instant midnightThisMorning() {
+        return Instant.now()
+                      .atZone(ZoneId.systemDefault())
+                      .withHour(0)
+                      .withMinute(0)
+                      .withSecond(0)
+                      .withNano(0)
+                      .toInstant();
     }
 }
