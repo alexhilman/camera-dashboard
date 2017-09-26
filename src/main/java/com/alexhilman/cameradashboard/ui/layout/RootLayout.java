e2073726d7d00b84702dae33e354292dcdcb1dba
Alex Hilman <alex.hilman@gmail.com>
2017-09-25 22:16:18 -0600

    Disabling the cameras button for now

--- src/main/java/com/alexhilman/cameradashboard/ui/layout/RootLayout.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/layout/RootLayout.java
@@ -1,6 +1,9 @@
 package com.alexhilman.cameradashboard.ui.layout;
 
-import com.alexhilman.cameradashboard.ui.view.*;
+import com.alexhilman.cameradashboard.ui.view.ClassNavigator;
+import com.alexhilman.cameradashboard.ui.view.Dashboard;
+import com.alexhilman.cameradashboard.ui.view.Movies;
+import com.alexhilman.cameradashboard.ui.view.ViewContainer;
 import com.google.inject.Inject;
 import com.vaadin.guice.annotation.UIScope;
 import com.vaadin.icons.VaadinIcons;
@@ -36,10 +39,10 @@ public class RootLayout extends HorizontalLayout {
         homeButton.addStyleNames(ValoTheme.BUTTON_HUGE, "circle-button");
         actions.addComponent(homeButton);
 
-        final Button camerasButton = new Button(VaadinIcons.MOVIE);
-        camerasButton.addClickListener(event -> ClassNavigator.navigateTo(Cameras.class));
-        camerasButton.addStyleNames(ValoTheme.BUTTON_HUGE, "circle-button");
-        actions.addComponent(camerasButton);
+//        final Button camerasButton = new Button(VaadinIcons.MOVIE);
+//        camerasButton.addClickListener(event -> ClassNavigator.navigateTo(Cameras.class));
+//        camerasButton.addStyleNames(ValoTheme.BUTTON_HUGE, "circle-button");
+//        actions.addComponent(camerasButton);
 
         final Button moviesButton = new Button(VaadinIcons.FILM);
         moviesButton.addClickListener(event -> ClassNavigator.navigateTo(Movies.class));
