b4168d3dfd55219fbff1a5194b1018144ba8cc9f
Alex Hilman <alex.hilman@gmail.com>
2018-02-04 15:31:28 -0700

    Removing the live view - it's not working :P

--- src/main/java/com/alexhilman/cameradashboard/ui/view/layout/RootLayout.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/view/layout/RootLayout.java
@@ -1,11 +1,18 @@
 package com.alexhilman.cameradashboard.ui.view.layout;
 
-import com.alexhilman.cameradashboard.ui.view.*;
+import com.alexhilman.cameradashboard.ui.view.ClassNavigator;
+import com.alexhilman.cameradashboard.ui.view.Dashboard;
+import com.alexhilman.cameradashboard.ui.view.Movies;
+import com.alexhilman.cameradashboard.ui.view.ViewContainer;
 import com.google.inject.Inject;
 import com.vaadin.guice.annotation.UIScope;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.shared.ui.MarginInfo;
-import com.vaadin.ui.*;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
 /**
@@ -36,10 +43,10 @@ public class RootLayout extends HorizontalLayout {
         homeButton.addStyleNames(ValoTheme.BUTTON_HUGE, "circle-button");
         actions.addComponent(homeButton);
 
-        final Button camerasButton = new Button(VaadinIcons.MOVIE);
-        camerasButton.addClickListener(event -> ClassNavigator.navigateTo(Cameras.class));
-        camerasButton.addStyleNames(ValoTheme.BUTTON_HUGE, "circle-button");
-        actions.addComponent(camerasButton);
+//        final Button camerasButton = new Button(VaadinIcons.MOVIE);
+//        camerasButton.addClickListener(event -> ClassNavigator.navigateTo(Cameras.class));
+//        camerasButton.addStyleNames(ValoTheme.BUTTON_HUGE, "circle-button");
+//        actions.addComponent(camerasButton);
 
         final Button moviesButton = new Button(VaadinIcons.FILM);
         moviesButton.addClickListener(event -> ClassNavigator.navigateTo(Movies.class));
