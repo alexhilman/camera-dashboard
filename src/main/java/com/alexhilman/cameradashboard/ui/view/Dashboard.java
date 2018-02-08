87d9c252ecac709ba99ecc85a401618e4477e602
Alex Hilman <alex.hilman@gmail.com>
2018-02-08 09:33:15 -0700

    Adding memory usage to dashboard. Refreshing progress bars on a schedule.

--- src/main/java/com/alexhilman/cameradashboard/ui/view/Dashboard.java
+++ src/main/java/com/alexhilman/cameradashboard/ui/view/Dashboard.java
@@ -6,28 +6,45 @@ import com.google.inject.Inject;
 import com.vaadin.guice.annotation.GuiceView;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewBeforeLeaveEvent;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.Sizeable;
-import com.vaadin.ui.*;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.ProgressBar;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import io.reactivex.Flowable;
 import io.reactivex.schedulers.Schedulers;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  */
 @GuiceView("")
 public class Dashboard implements View {
     private static final Logger LOG = LogManager.getLogger(Dashboard.class);
+    private static final NumberFormat NUMBER_FORMAT = new DecimalFormat("#,##0.0");
 
     private final MovieFileManager movieFileManager;
     private final MovieViewHelper movieViewHelper;
+    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     private VerticalLayout rootLayout;
-    private ProgressBar progressBar;
+    private ProgressBar storageBar;
     private Panel todayMovies;
+    private ProgressBar memoryBar;
 
     @Inject
     public Dashboard(final MovieFileManager movieFileManager,
@@ -36,15 +53,26 @@ public class Dashboard implements View {
         this.movieViewHelper = movieViewHelper;
     }
 
+    public static String humanReadableByteCount(long bytes) {
+        final int unit = 1000;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), "kMGTPE".charAt(exp - 1));
+    }
+
     @Override
     public Component getViewComponent() {
         rootLayout = new VerticalLayout();
         rootLayout.setMargin(true);
         rootLayout.setSpacing(true);
 
-        progressBar = new ProgressBar(0f);
-        progressBar.setWidth(100, Sizeable.Unit.PERCENTAGE);
-        rootLayout.addComponent(progressBar);
+        storageBar = new ProgressBar(0f);
+        storageBar.setWidth(100, Sizeable.Unit.PERCENTAGE);
+        rootLayout.addComponent(storageBar);
+
+        memoryBar = new ProgressBar(0f);
+        memoryBar.setWidth(100, Sizeable.Unit.PERCENTAGE);
+        rootLayout.addComponent(memoryBar);
 
         todayMovies = new Panel("Today's Movies");
         todayMovies.setIcon(VaadinIcons.FILM);
@@ -55,21 +83,58 @@ public class Dashboard implements View {
     }
 
     @Override
+    public void beforeLeave(final ViewBeforeLeaveEvent event) {
+        executorService.shutdownNow();
+    }
+
+    @Override
     public void enter(final ViewChangeListener.ViewChangeEvent event) {
-        final long usableSpace = movieFileManager.getUsableSpace();
-        final long totalSpace = movieFileManager.getTotalSpace();
-        final long usedSpace = totalSpace - usableSpace;
-
-        final float progress = (float) ((double) usedSpace / (double) totalSpace);
-        progressBar.setValue(progress);
-        progressBar.setCaption("Storage space: " + humanReadableByteCount(usedSpace) +
-                                       " used out of " + humanReadableByteCount(totalSpace) +
-                                       " (" + humanReadableByteCount(usableSpace) + " free)");
-
-        todayMovies.setContent(buildLoadingSpinner());
-
         final UI ui = UI.getCurrent();
 
+        executorService.scheduleWithFixedDelay(
+                () -> {
+                    if (!ui.isAttached()) {
+                        executorService.shutdownNow();
+                        return;
+                    }
+
+                    final long usableSpace = movieFileManager.getUsableSpace();
+                    final long totalSpace = movieFileManager.getTotalSpace();
+                    final long usedSpace = totalSpace - usableSpace;
+                    ui.access(() -> {
+                        final float progress = (float) ((double) usedSpace / (double) totalSpace);
+                        storageBar.setValue(progress);
+                        storageBar.setCaption("Storage space: " + humanReadableByteCount(usedSpace) +
+                                                      " used out of " + humanReadableByteCount(totalSpace) +
+                                                      " (" + humanReadableByteCount(usableSpace) + " free)");
+                    });
+                },
+                0,
+                10,
+                TimeUnit.SECONDS
+        );
+
+        executorService.scheduleWithFixedDelay(
+                () -> {
+                    if (!ui.isAttached()) {
+                        executorService.shutdownNow();
+                        return;
+                    }
+
+                    final Runtime runtime = Runtime.getRuntime();
+                    ui.access(() -> {
+                        final long usedMemory = runtime.totalMemory() - runtime.freeMemory();
+                        final long totalMemory = runtime.maxMemory();
+                        final double usedMemoryMB = usedMemory / 1024.0 / 1024.0;
+                        memoryBar.setValue((float) usedMemory / (float) totalMemory);
+                        memoryBar.setCaption("Used memory: " + NUMBER_FORMAT.format(usedMemoryMB) + "MB");
+                    });
+                },
+                0,
+                750,
+                TimeUnit.MILLISECONDS);
+
+        todayMovies.setContent(buildLoadingSpinner());
         Flowable.fromCallable(() -> movieFileManager.getMoviesInRange(midnightThisMorning(), Instant.now()))
                 .map(movieViewHelper::buildPostersFor)
                 .subscribeOn(Schedulers.computation())
@@ -104,13 +169,6 @@ public class Dashboard implements View {
         layout.setComponentAlignment(progressBar, Alignment.MIDDLE_CENTER);
 
         return layout;
-    }
-
-    public static String humanReadableByteCount(long bytes) {
-        final int unit = 1000;
-        if (bytes < unit) return bytes + " B";
-        int exp = (int) (Math.log(bytes) / Math.log(unit));
-        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), "kMGTPE".charAt(exp - 1));
     }
 
     private Instant midnightThisMorning() {
